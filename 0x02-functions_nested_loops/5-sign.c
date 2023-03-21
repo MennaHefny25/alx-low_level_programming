dd7eed9cdea946c671fd7fc289fcd87b364e5ea8
MennaHefny25 <mennahefny25@gmail.com>
2023-03-21 03:33:57 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,9 +11,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_puchar('+');
 		return (1);
-	if (n == 0)
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
 		return (0);
-	if (n < 0)
+	}
+	else
+	{
+		_putchar('-');
 		return (-1);
+	}
 }
