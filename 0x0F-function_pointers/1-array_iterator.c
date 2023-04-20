37977947af0ffcb70114fb573a910a42d2956d07
Menna Hefny <Menna Hefny>
2023-04-20 03:36:54 +0200

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int iter;
+	unsigned int i;
 
 	if (!array || !action)
 		return;
 
-	for (iter = 0; iter < size; iter)
-		actoin(array[iter]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
